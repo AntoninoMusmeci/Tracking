cc5a46f6d216416cd0654fd3f22c3b6c49e4ab4b
Antonino Musmeci <nino.musmeci@gmail.com>
2022-09-20 23:04:57 +0200

    add empty route pages

--- frontend/src/pages/Pages.jsx
+++ frontend/src/pages/Pages.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { Routes, Route} from "react-router-dom"
-import Diary from './Diary'
-import HomePage from './HomePage'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Diary from "./Diary";
+import HomePage from "./HomePage";
+import NavBar from "../components/NavBar";
+import Dashboard from "./Dashboard";
+import Recipes from "./Recipes";
 function Pages() {
   return (
-    <Routes>
+    <>
+      <NavBar />
+      <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/diary/:date" element={<Diary />} />
-    </Routes>
-  )
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/recipes" element={<Recipes />} />
+      </Routes>
+    </>
+  );
 }
 
-export default Pages
+export default Pages;
---
+++ frontend/src/pages/Recipes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Recipes() {
+  return (
+    <div>recipes</div>
+  )
+}
+
+export default Recipes
